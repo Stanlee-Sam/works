b7340bb4b936b1d76cfb913539a7d11dab0c8c70
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-07-14 01:27:56 +0300

    ...

--- src/Works/workData.js
+++ src/Works/workData.js
@@ -1,27 +1,27 @@
 const worksData = [
     {
-        imgSrc: "../src/assets/works/dashboard.jpeg",
+        imgSrc: "../assets/works/dashboard.jpeg",
         title: "Designing Dashboards",
         year: "2020",
         category: "Dashboard",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati rerum ratione voluptas porro blanditiis recusandae qui quidem deleniti labore deserunt, nemo suscipit. Amet id, ipsam temporibus, non itaque ipsum dolore est a nesciunt reprehenderit excepturi nostrum. Eos maiores asperiores possimus!"
       },
       {
-        imgSrc: "../src/assets/works/portrait.jpeg",
+        imgSrc: "../assets/works/portrait.jpeg",
         title: "Vibrant Portraits of 2020",
         year: "2018",
         category: "Illustration",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati rerum ratione voluptas porro blanditiis recusandae qui quidem deleniti labore deserunt, nemo suscipit. Amet id, ipsam temporibus, non itaque ipsum dolore est a nesciunt reprehenderit excepturi nostrum. Eos maiores asperiores possimus!"
       },
       {
-        imgSrc: "../src/assets/works/typography.jpeg",
+        imgSrc: "../assets/works/typography.jpeg",
         title: "36 Days of Malayalam type",
         year: "2018",
         category: "Typography",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati rerum ratione voluptas porro blanditiis recusandae qui quidem deleniti labore deserunt, nemo suscipit. Amet id, ipsam temporibus, non itaque ipsum dolore est a nesciunt reprehenderit excepturi nostrum. Eos maiores asperiores possimus!"
       },
       {
-        imgSrc: "../src/assets/works/component.png",
+        imgSrc: "../assets/works/component.png",
         title: "Components",
         year: "2018",
         category: "Components, Design",
